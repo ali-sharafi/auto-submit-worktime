39b190388ad5d3317f88b3ef53396d06463006b4
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-17 22:54:18 +0430

    Change structure

--- src/tools/tools.ts
+++
@@ -1,7 +0,0 @@
-let res = document.removeEventListener('mouseup', () => { });
-console.log('res: ', res);
-document.addEventListener('mouseup', (e) => {
-    let selected = document.getSelection();
-    if (selected)
-        console.log('selected: ', selected.toString());
-})
