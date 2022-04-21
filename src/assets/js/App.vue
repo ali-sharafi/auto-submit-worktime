1626ffcd4f66ac3bc652ff350542fc690d05865e
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-21 09:11:31 +0430

    Complete UI

--- src/assets/js/App.vue
+++ src/assets/js/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <el-tabs class="main-card" type="card" @tab-click="handleClick">
+  <el-tabs class="main-card" type="card">
     <el-tab-pane label="Learners">
       <el-row
         class="translation-item"
         v-for="(item, index) in learners"
         :key="index"
+        @click.native="itemClicked(item)"
+        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
       >
         <span v-html="item"></span>
       </el-row>
     </el-tab-pane>
-    <el-tab-pane label="Config">Config</el-tab-pane>
-    <el-tab-pane label="Role">Role</el-tab-pane>
-    <el-tab-pane label="Task">Task</el-tab-pane>
+    <el-tab-pane label="Examples">
+      <el-row
+        class="translation-item"
+        v-for="(item, index) in examples"
+        :key="index"
+        @click.native="itemClicked(item)"
+        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
+      >
+        <span v-html="item"></span>
+      </el-row>
+    </el-tab-pane>
+    <el-tab-pane label="Meanings">
+      <el-row
+        class="translation-item"
+        v-for="(item, index) in meanings"
+        :key="index"
+        @click.native="itemClicked(item)"
+        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
+      >
+        <span v-html="item"></span>
+      </el-row>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
@@ -30,38 +51,55 @@ export default {
   data() {
     return {
       activeName: "first",
-      items: {
-        learners: [],
-        examples: [],
-        meanings: [],
-      },
+      items: [],
     };
   },
   computed: {
     learners() {
       return this.translation.learners;
     },
+    examples() {
+      return this.translation.examples;
+    },
+    meanings() {
+      return this.translation.meanings;
+    },
+  },
+  mounted() {
+    console.log("mounted: ", this.translation);
   },
   methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
+    isIncluesSeleted(item) {
+      return this.items.indexOf(item) !== -1;
+    },
+    itemClicked(item) {
+      if (this.isIncluesSeleted(item)) {
+        let itemIndex = this.items.indexOf(item);
+        this.items.splice(itemIndex, 1);
+      } else this.items.push(item);
+      console.log("item: ", item.replace(/<[^>]*>/g, ""));
     },
   },
 };
 </script>
 
 <style scoped>
+.translator-wraper [role="tablist"] {
+  position: fixed;
+  transform: translateY(-40px);
+}
 .translation-item {
   border: 1px solid #ccc;
   border-radius: 5px;
   padding: 4px;
   margin: 0 0 4px 4px;
+  cursor: pointer;
 }
 
 .main-card {
   overflow-y: scroll;
 }
-.main-card::-webkit-scrollbar{
+.main-card::-webkit-scrollbar {
   display: none;
 }
 </style>
