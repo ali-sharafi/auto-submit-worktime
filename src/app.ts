9a76a5627c732aed95af91e475c629fabb297947
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-21 09:11:40 +0430

    Update properties structure

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import App from './assets/js/App.vue';
 class Translator {
     wordsList: Array<string>;
 
-    constructor() {
+    constructor(private appComponent: Vue | null = null, private translation: TranslateResult | null = null) {
         this.registerListener();
         this.wordsList = [];
     }
@@ -19,17 +19,21 @@ class Translator {
         let selected = document.getSelection()?.toString().trim();
         if (selected && this.isSelectedValid(selected)) {
             this.wordsList.push(selected);
-            let translation = await this.translateWord(selected);
+            this.translation = await this.translateWord(selected);
             this.createModal();
-            this.mountDataToPage(translation);
+            if (this.translation)
+                this.mountDataToPage();
         }
     }
 
-    mountDataToPage(translation: TranslateResult | null) {
-        new Vue({
-            el: '#main-wraper',
-            render: h => h(App, { props: { translation: translation } })
-        })
+    mountDataToPage() {
+        if (!this.appComponent) {
+            this.appComponent = new Vue({
+                el: '#main-wraper',
+                render: h => h(App, { props: { translation: this.translation } })
+            });
+        }
+        console.log('vue: ', this.appComponent);
     }
 
     async translateWord(word: string): Promise<TranslateResult | null> {
