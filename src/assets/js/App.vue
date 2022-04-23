58f9ca33b2dfa784bea89b72d4647cbe649fc6b4
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-23 08:55:25 +0430

    Add action buttons

--- src/assets/js/App.vue
+++ src/assets/js/App.vue
@@ -1,43 +1,56 @@
 <template>
-  <el-tabs class="main-card" type="card">
-    <el-tab-pane label="Learners">
-      <el-row
-        class="translation-item"
-        v-for="(item, index) in learners"
-        :key="index"
-        @click.native="itemClicked(item)"
-        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
-      >
-        <span v-html="item"></span>
-      </el-row>
-    </el-tab-pane>
-    <el-tab-pane label="Examples">
-      <el-row
-        class="translation-item"
-        v-for="(item, index) in examples"
-        :key="index"
-        @click.native="itemClicked(item)"
-        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
-      >
-        <span v-html="item"></span>
-      </el-row>
-    </el-tab-pane>
-    <el-tab-pane label="Meanings">
-      <el-row
-        class="translation-item"
-        v-for="(item, index) in meanings"
-        :key="index"
-        @click.native="itemClicked(item)"
-        :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
-      >
-        <span v-html="item"></span>
-      </el-row>
-    </el-tab-pane>
-  </el-tabs>
+  <div :class="{ 'translator-wraper': true, hidden: hidden }">
+    <el-tabs class="main-card" type="card">
+      <el-tab-pane label="Learners">
+        <el-row
+          class="translation-item"
+          v-for="(item, index) in learners"
+          :key="index"
+          @click.native="itemClicked(item)"
+          :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
+        >
+          <span v-html="item"></span>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="Examples">
+        <el-row
+          class="translation-item"
+          v-for="(item, index) in examples"
+          :key="index"
+          @click.native="itemClicked(item)"
+          :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
+        >
+          <span v-html="item"></span>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="Meanings">
+        <el-row
+          class="translation-item"
+          v-for="(item, index) in meanings"
+          :key="index"
+          @click.native="itemClicked(item)"
+          :style="`background-color:${isIncluesSeleted(item) ? '#98e8f5' : ''}`"
+        >
+          <span v-html="item"></span>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
+    <el-button class="save-button" type="success" @click="save">Save</el-button>
+    <el-button class="close-button" type="danger" circle @click="close"
+      >X</el-button
+    >
+  </div>
 </template>
 
 <script>
-import { Tabs, TabPane, Row, Checkbox, CheckboxGroup } from "element-ui";
+import {
+  Tabs,
+  TabPane,
+  Row,
+  Checkbox,
+  CheckboxGroup,
+  Button,
+} from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 export default {
   components: {
@@ -46,29 +59,35 @@ export default {
     "el-row": Row,
     "el-checkbox": Checkbox,
     "el-checkbox-group": CheckboxGroup,
+    "el-button": Button,
   },
   props: ["translation"],
   data() {
     return {
       activeName: "first",
       items: [],
+      localTranslation: this.translation,
+      hidden: false,
     };
   },
   computed: {
     learners() {
-      return this.translation.learners;
+      return this.localTranslation.learners;
     },
     examples() {
-      return this.translation.examples;
+      return this.localTranslation.examples;
     },
     meanings() {
-      return this.translation.meanings;
+      return this.localTranslation.meanings;
     },
   },
-  mounted() {
-    console.log("mounted: ", this.translation);
-  },
   methods: {
+    save() {
+      console.log("saved");
+    },
+    close() {
+      this.hidden = true;
+    },
     isIncluesSeleted(item) {
       return this.items.indexOf(item) !== -1;
     },
@@ -84,6 +103,22 @@ export default {
 </script>
 
 <style scoped>
+.close-button {
+  position: absolute;
+  left: -30px;
+  top: -20px;
+  z-index: 20;
+}
+.save-button {
+  position: absolute;
+  left: -67px;
+  top: 260px;
+  border-radius: 4px 0 0;
+}
+.hidden {
+  visibility: hidden;
+  opacity: 0;
+}
 .translator-wraper [role="tablist"] {
   position: fixed;
   transform: translateY(-40px);
