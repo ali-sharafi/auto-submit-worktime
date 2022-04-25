eff5ae27fe87136638c5cec1c9429d8999df16ec
Ali Sharafi <a.sharafi1993@gmail.com>
2022-04-25 09:17:12 +0430

    Update append cycle

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -4,6 +4,8 @@ declare type TranslationPayload = {
     meanings: string;
     category: string;
     sheetID: string;
+    setID: string;
+    definitionsID: string;
 }
 
 chrome.runtime.onMessage.addListener(
@@ -16,33 +18,62 @@ chrome.runtime.onMessage.addListener(
 );
 
 function store(payload: TranslationPayload) {
-    chrome.identity.getAuthToken({ interactive: true }, (token) => {
-        let init = {
-            method: 'POST',
-            async: true,
-            body: JSON.stringify({
-                range: 'K1:N1',
-                values: [
-                    [
-                        payload.word,
-                        null,
-                        payload.meanings,
-                        payload.category
-                    ]
-                ]
-            }),
-            headers: {
-                Authorization: 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            },
-            'contentType': 'json'
-        };
-        fetch(
-            `https://sheets.googleapis.com/v4/spreadsheets/${payload.sheetID}/values/K1:N1:append?valueInputOption=USER_ENTERED`,
-            init)
-            .then(async (response) => {
-                console.log('response: ', await response.json());
-            }).catch(console.error)
-        console.log('paylod: ', payload, 'token: ', token);
+    chrome.identity.getAuthToken({ interactive: true }, async (token) => {
+        let appendRes = await append(payload, token);
+        console.log('appendRes: ', appendRes, 'token: ', token);
     })
+}
+
+async function append(payload: TranslationPayload, token: string) {
+    let init = {
+        method: 'POST',
+        async: true,
+        body: JSON.stringify({
+            range: 'L:O',
+            majorDimension: "ROWS",
+            values: [
+                [
+                    generateUID(),//vocabularyId
+                    payload.definitionsID,//originalDefinitionIds
+                    payload.word,//originalVocabularyText
+                    "ACTIVE",//originalVocabularyStatus
+                    payload.meanings,//originalDefinitions
+                    payload.category,//originalCategory
+                    0,//originalSpacedRepetitionLevel
+                    0,//originalWritingLevel
+                    false,//originalDisableWriting
+                    payload.setID,//originalSetId
+                    payload.word,//vocabularyText
+                    "ACTIVE",//vocabularyStatus
+                    payload.meanings,//definitions
+                    payload.category,//category
+                    0,//spacedRepetitionLevel
+                    0,//writingLevel
+                    false,//disableWriting
+                    payload.setID,//setId
+                    "NEW"//edited
+                ]
+            ]
+        }),
+        headers: {
+            Authorization: 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+    };
+    let response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${payload.sheetID}/values/L:O:append?valueInputOption=USER_ENTERED`,
+        init).catch(console.error)
+    if (response)
+        return await response.json();
+}
+
+function generateUID() {
+    var ID_LENGTH = 5;
+    var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var rtn = '';
+    for (var i = 0; i < ID_LENGTH; i++) {
+        rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return rtn;
 }
