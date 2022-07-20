371529246f5a916de0b8de05703c0665c840dbf7
Ali Sharafi <a.sharafi1993@gmail.com>
2022-07-20 20:15:29 +0430

    Register popup files to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,9 @@ const Dotenv = require('dotenv-webpack');
 mix
     .js("src/app.ts", "dist/js/app.js")
     .js("src/serviceWorker.ts", "dist/js/main.js")
+    .js("src/assets/js/popup/main.js", "dist/popup.js")
     .sass('src/scss/app.scss', 'dist/css/app.css')
+    .sass('src/assets/scss/popup.scss', 'dist/popup.css')
     .webpackConfig({
         resolve: {
             extensions: ['.ts', '.js', '.json', '.vue']
